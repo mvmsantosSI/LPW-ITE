bd05fc87ead4b5c144c1467e15d54b1bd216f2d8
Marcos Santos <mvmsantos.si@gmail.com>
2023-03-09 21:33:42 -0300

    feat: Added exercs multiplication table

---
+++ exercicios/exerc2.php
@@ -0,0 +1,28 @@
+<?php
+
+    if( isset($variavel))
+    {
+        echo $variavel;
+    }else
+    {
+        echo "Variavel não existe";
+    }
+
+?>
+
+<form action="exerc.php" method="get">
+    <span>Entre com o numero:</span>
+    <input type="text" name="txtNumero" id="" />
+    <input type="submit" value="Enviar" />
+</form>
+
+<?php
+    if ( isset($_GET['txtNumero'])) {
+        for($i = 0; $i <= 10; $i++)
+        {
+            echo $_GET['txtNumero'] . 'x' . $i . " = ";
+            echo $_GET['txtNumero'] * $i;
+            echo "<br/>";
+        }
+    }
+?>
