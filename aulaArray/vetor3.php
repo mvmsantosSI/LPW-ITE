dc0b2441eebe37020b9532fb990fc80d9004d787
Marcos Santos <mvmsantos.si@gmail.com>
2023-03-09 21:10:31 -0300

    Added array examples
    Added array examples

---
+++ aulaArray/vetor3.php
@@ -0,0 +1,25 @@
+<?php
+
+    $variavel = array(
+        0 => 'Branco',
+        1 => 'Azul',
+        2 => 'Amarelo',
+        3 => 85
+    );
+
+    $variavel [] = 'Preto';
+
+    $carros = array(
+        'Volks' => 'Fusca',
+        'Fiat' => 'Uno',
+        'Ford' => 'Fiesta'
+    );
+
+    //echo $carros['Fiat'];
+
+    //var_dump(print_r($carros, true));
+
+    //$carros['Ferrari'] = '350';
+    
+    var_dump(print_r($carros, true));
+?>
---
+++ aulaArray/vetor4.php
@@ -0,0 +1,32 @@
+<?php
+
+//TABELA NO EXCELL
+
+    $banco = array(
+        0 => array(
+            'nome' => 'Vanderleia',
+            'idade' => 300,
+            'estado' => 'RJ'
+        ),
+        1 => array(
+            'nome' => 'Valdisney',
+            'idade' => 25,
+            'estado' => 'SP'
+        ),
+        2 => array(
+            'nome' => 'Eva',
+            'idade' => 25,
+            'estado' => 'PB'
+        ),
+        3 => array(
+            'nome' => 'Adao',
+            'idade' => 25,
+            'estado' => 'AC'
+        )
+    );
+
+    MOSTRA VALDISNEY
+    echo $banco[1]["nome"]
+
+
+?>
